55c004c46382122b74566249edd4448be9c422af
shootnicks <pg.andriyanov@gmail.com>
2022-09-07 16:43:24 +0300

    I finished the 6th lesson

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            Simple HTML
+            <div className="bla">
+                <ul>
+                    <li> 123</li>
+                    <li> 456</li>
+                    <li> 789</li>
+                </ul>
+            </div>
+            {/*<header className="App-header">*/}
+            {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+            {/*  <p>*/}
+            {/*    Ну что, погнали заново учить React. А для начала редактируем <code>src/App.js</code>.*/}
+            {/*  </p>*/}
+            {/*  <a*/}
+            {/*    className="App-link"*/}
+            {/*    href="https://reactjs.org"*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*  >*/}
+            {/*    Learn React*/}
+            {/*  </a>*/}
+            {/*</header>*/}
+        </div>
+    );
 }
 
 export default App;
